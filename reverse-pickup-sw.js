528093adcea00aeafa3bfeddb24a1e2cc89256bd
Gaman-Cheung <farmerspring2020@gmail.com>
2026-09-23 20:29:33 +0800

    Add files via upload

---
+++ reverse-pickup-sw.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const CACHE_PREFIX = 'reverse-pickup-pwa-';
+const CACHE_NAME = CACHE_PREFIX + 'v1';
+const APP_SHELL = [
+  './倒车接人计算器.html',
+  './reverse-pickup.webmanifest',
+  './reverse-pickup-icon.svg',
+  './reverse-pickup-icon-192.png',
+  './reverse-pickup-icon-512.png',
+  './reverse-pickup-icon-512-maskable.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys
+        .filter((key) => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
+        .map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          if (response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put('./倒车接人计算器.html', copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match('./倒车接人计算器.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      if (response.ok) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+      }
+      return response;
+    }))
+  );
+});
